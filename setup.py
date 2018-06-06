f116506b73b67cd0139462b50e3e70d5be85ffaf
Alvin Xiaochuan Han <hanxc@bii.a-star.edu.sg>
2018-06-06 12:07:59 +0800

    v0.1

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------------- #
+# Phylogenetic Clustering by Linear Integer Programming (PhyCLIP) #
+# Authors: Alvin X. Han and Edyth Parker                          #
+# --------------------------------------------------------------- #
+
+import os
+from setuptools import setup
+
+setup(
+    name = 'PhyCLIP',
+    version = '0.1',
+    author = 'Alvin X. Han and Edyth Parker',
+    author_email = 'hanxc@bii.a-star.edu.sg',
+    description = ('Phylogenetic clustering by linear integer programming.'),
+    keywords = 'Phylogenetics, Clustering',
+    url = 'https://github.com/alvinxhan/PhyCLIP',
+    packages = ['phyclip_modules'],
+    scripts = ['bin/phyclip.py']
+)
